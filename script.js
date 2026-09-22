ed0af572462607f6a2d848fe22581bea695a70d3
rynn-ie <legalvin9@gmail.com>
2026-09-22 19:27:49 -0400

    Did JS, need to test still

--- script.js
+++ script.js
@@ -1,72 +1,165 @@
-inputs
+// inputs and outputs
 
-customWord
-theWord
-circle1Color
-circle2Color
-circle3Color
+let customWord = document.getElementById("customWord")
+let circle1Color = document.getElementById("circle1Color")
+let circle1ColorOut = document.querySelector("output[for=circle1Color]")
+let circle2Color = document.getElementById("circle2Color")
+let circle2ColorOut = document.querySelector("output[for=circle2Color]")
+let circle3Color = document.getElementById("circle3Color")
+let circle3ColorOut = document.querySelector("output[for=circle3Color]")
 
-inputs for cats
+// inputs for cats
 
-fatcatScale
-fatcatMoveX
-fatcatMoveY
+let fatcatScale = document.getElementById("fatcatScale")
+let fatcatMoveX = document.getElementById("fatcatMoveX")
+let fatcatMoveY = document.getElementById("fatcatMoveY")
 
-hatcatScale
-hatcatMoveX
-hatcatMoveY
+let hatcatScale = document.getElementById("hatcatScale")
+let hatcatMoveX = document.getElementById("hatcatMoveX")
+let hatcatMoveY = document.getElementById("hatcatMoveY")
 
-scaredcatScale
-scaredcatMoveX
-scaredcatMoveY
+let scaredcatScale = document.getElementById("scaredcatScale")
+let scaredcatMoveX = document.getElementById("scaredcatMoveX")
+let scaredcatMoveY = document.getElementById("scaredcatMoveY")
 
-elements
+let buffcatScale = document.getElementById("buffcatScale")
+let buffcatMoveX = document.getElementById("buffcatMoveX")
+let buffcatMoveY = document.getElementById("buffcatMoveY")
 
-circle1
-circle2
-circle3
-fatcat
-hatcat
-isthiscat
-buffcat
-scaredcat
-studycat
+let studycatScale = document.getElementById("studycatScale")
+let studycatMoveX = document.getElementById("studycatMoveX")
+let studycatMoveY = document.getElementById("studycatMoveY")
 
+let isthiscatScale = document.getElementById("isthiscatScale")
+let isthiscatMoveX = document.getElementById("isthiscatMoveX")
+let isthiscatMoveY = document.getElementById("isthiscatMoveY")
 
+// elements
 
+let theWord = document.getElementById("theWord")
+let circle1 = document.getElementById("circle1")
+let circle2 = document.getElementById("circle2")
+let circle3 = document.getElementById("circle3")
+let fatcat = document.getElementById("fatcat")
+let hatcat = document.getElementById("hatcat")
+let isthiscat = document.getElementById("isthiscat")
+let buffcat = document.getElementById("buffcat")
+let scaredcat = document.getElementById("scaredcat")
+let studycat = document.getElementById("studycat")
 
+// functions - change text, change color, update outputs, change scale, change position
 
+function changeText(object, textInput) {
+    object.textContent = textInput.value
+}
 
+function changeColor(object, color) {
+    object.style.backgroundColor = color.value
+}
 
+function updateOutput(object, input) {
+    object.textContent = input.value
+}
 
+function changeScale(object, scale) {
+    object.style.width = scale.value + "px"
+}
 
+function movePosition(object, xPos, yPos) {
+    object.style.transform = `translate(${xPos.value}px, ${yPos.value}px)`
+}
 
+// event listeners to run the functions
 
+customWord.addEventListener("input", function(){
+    changeText(theWord, customWord);
+})
 
+//circles
 
+circle1Color.addEventListener("input", function(){
+    changeColor(circle1, circle1Color);
+    updateOutput(circle1ColorOut, circle1Color);
+})
 
+circle2Color.addEventListener("input", function(){
+    changeColor(circle2, circle2Color);
+    updateOutput(circle2ColorOut, circle2Color);
+})
 
+circle3Color.addEventListener("input", function(){
+    changeColor(circle3, circle3Color);
+    updateOutput(circle3ColorOut, circle3Color);
+})
 
+//cats grow
 
+fatcatScale.addEventListener("input", function(){
+    changeScale(fatcat, fatcatScale);
+})
+hatcatScale.addEventListener("input", function(){
+    changeScale(hatcat, hatcatScale);
+})
+scaredcatScale.addEventListener("input", function(){
+    changeScale(scaredcat, scaredcatScale);
+})
+buffcatScale.addEventListener("input", function(){
+    changeScale(buffcat, buffcatScale);
+})
+studycatScale.addEventListener("input", function(){
+    changeScale(studycat, studycatScale);
+})
+isthiscatScale.addEventListener("input", function(){
+    changeScale(isthiscat, isthiscatScale);
+})
 
+//cats move
 
+fatcatMoveX.addEventListener("input", function(){
+    movePosition(fatcat, fatcatMoveX, fatcatMoveY);
+})
+fatcatMoveY.addEventListener("input", function(){
+    movePosition(fatcat, fatcatMoveX, fatcatMoveY);
+})
 
+hatcatMoveX.addEventListener("input", function(){
+    movePosition(hatcat, hatcatMoveX, hatcatMoveY);
+})
+hatcatMoveY.addEventListener("input", function(){
+    movePosition(hatcat, hatcatMoveX, hatcatMoveY);
+})
 
+scaredcatMoveX.addEventListener("input", function(){
+    movePosition(scaredcat, scaredcatMoveX, scaredcatMoveY);
+})
+scaredcatMoveY.addEventListener("input", function(){
+    movePosition(scaredcat, scaredcatMoveX, scaredcatMoveY);
+})
 
+buffcatMoveX.addEventListener("input", function(){
+    movePosition(buffcat, buffcatMoveX, buffcatMoveY);
+})
+buffcatMoveY.addEventListener("input", function(){
+    movePosition(buffcat, buffcatMoveX, buffcatMoveY);
+})
 
+studycatMoveX.addEventListener("input", function(){
+    movePosition(studycat, studycatMoveX, studycatMoveY);
+})
+studycatMoveY.addEventListener("input", function(){
+    movePosition(studycat, studycatMoveX, studycatMoveY);
+})
 
-
-
-
-
-
-
-
-
+isthiscatMoveX.addEventListener("input", function(){
+    movePosition(isthiscat, isthiscatMoveX, isthiscatMoveY);
+})
+isthiscatMoveY.addEventListener("input", function(){
+    movePosition(isthiscat, isthiscatMoveX, isthiscatMoveY);
+})
 
 
 /* 
-        _   _  ___ _____ _____ ____          
+         _   _  ___ _____ _____ ____          
         | \ | |/ _ \_   _| ____/ ___|         
  _____  |  \| | | | || | |  _| \___ \   _____ 
 |_____| | |\  | |_| || | | |___ ___) | |_____|
